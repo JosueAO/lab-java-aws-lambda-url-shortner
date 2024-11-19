0fae8f2a280b8754d265cd8164016284c0c1e47a
Josue A. Oliveira <josueao.mobile@gmail.com>
2024-11-19 12:19:18 -0300

    feat: Integrate AWS S3, generate UUID for shortened URLs, and save data as JSON

---
+++ src/main/java/com/josueao/creatUrlShortner/UrlData.java
@@ -0,0 +1,14 @@
+package com.josueao.creatUrlShortner;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+    private String originalUrl;
+    private Long expirationTime;
+}
